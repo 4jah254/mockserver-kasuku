b9329e345f6e61965c1e09a2b99aa515cde24e82
Japheth Korir <japhethkorir@gmail.com>
2022-12-31 09:26:24 +0300

    Add GET controller for returning books
    - Add web dependency

---
+++ src/main/kotlin/personal/zukanori/mockserverkasuku/BooksInfoController.kt
@@ -0,0 +1,19 @@
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import personal.zukanori.models.BooksDto
+
+@RestController
+@RequestMapping("books/size")
+class BooksInfoController {
+
+    @GetMapping("/{id}", produces = ["application/json"])
+    fun getBook(@PathVariable id: Int): BooksDto {
+        return findBookById(id)
+    }
+
+    private fun findBookById(id: Int): BooksDto {
+        return BooksDto("A5", 70, "70 GSM")
+    }
+}
---
+++ src/main/kotlin/personal/zukanori/models/BooksDto.kt
@@ -0,0 +1,6 @@
+package personal.zukanori.models
+
+data class BooksDto(val size: String, val numOfPages: Int, val paperQuality: String)
+
+
+// paperQuality can even come from an enum.
