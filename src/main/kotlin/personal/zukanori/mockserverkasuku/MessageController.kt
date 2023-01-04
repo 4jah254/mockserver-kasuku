f3dde91d9e5cd6d6e4217cfc48f54fc293f72d35
Japheth Korir <japhethkorir@gmail.com>
2023-01-04 16:55:26 +0300

    Add REST controller that renders JSON response

---
+++ src/main/kotlin/personal/zukanori/mockserverkasuku/MessageController.kt
@@ -0,0 +1,15 @@
+package personal.zukanori.mockserverkasuku
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import personal.zukanori.models.Message
+
+@RestController
+class MessageController {
+    @GetMapping("/")
+    fun index() = listOf(
+        Message("1", "Hello!"),
+        Message("2", "Bonjour!"),
+        Message("3", "Privet!"),
+    )
+}
---
+++ src/main/kotlin/personal/zukanori/models/Message.kt
@@ -0,0 +1,3 @@
+package personal.zukanori.models
+
+data class Message(val id: String?, val text: String)
